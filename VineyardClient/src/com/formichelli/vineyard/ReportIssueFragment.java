950dc0e923051d40eb8d8e61c0cf70131ad05822
Daniele Formichelli <daniele.formichelli@gmail.com>
2014-04-14 20:58:18 +0200

    Added image capture in report issue fragment.

--- VineyardClient/src/com/formichelli/vineyard/ReportIssueFragment.java
+++ VineyardClient/src/com/formichelli/vineyard/ReportIssueFragment.java
@@ -1,15 +1,30 @@
 package com.formichelli.vineyard;
 
+import com.formichelli.vineyard.entities.IssueTask;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class ReportIssueFragment extends Fragment {
+	public static final int REQUEST_IMAGE_CAPTURE = 1;
+
+	VineyardMainActivity activity;
+	IssueTask i;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -19,6 +34,17 @@ public class ReportIssueFragment extends Fragment {
 		// Inflate the layout for this fragment
 		return inflater.inflate(R.layout.fragment_report_issue, container,
 				false);
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+
+		activity = (VineyardMainActivity) getActivity();
+		activity.reportIssueFragment = this;
+
+		activity.findViewById(R.id.report_issue_add_photo).setOnClickListener(
+				dispatchTakePictureIntent);
 	}
 
 	@Override
@@ -32,17 +58,88 @@ public class ReportIssueFragment extends Fragment {
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 
-		if (item.getItemId() == R.id.action_report_issue_cancel) {
-			getActivity()
-					.getSupportFragmentManager()
-					.beginTransaction()
-					.replace(
-							R.id.container,
-							((VineyardMainActivity) getActivity()).issuesFragment)
-					.commit();
+		switch (item.getItemId()) {
+		case R.id.action_report_issue_cancel:
+			closeFragment();
 			return true;
+		case R.id.action_report_issue_send:
+			if (parseFields()) {
+				Toast.makeText(activity, "TODO: send issue to server",
+						Toast.LENGTH_LONG).show();
+				closeFragment();
+			}
+			return true;
+		default:
+			return false;
+		}
+	}
+
+	private void closeFragment() {
+		activity.getSupportFragmentManager()
+				.beginTransaction()
+				.replace(R.id.container,
+						((VineyardMainActivity) getActivity()).issuesFragment)
+				.commit();
+	}
+
+	private boolean parseFields() {
+		Activity activity = getActivity();
+		EditText t;
+		String s;
+
+		i = new IssueTask();
+
+		t = (EditText) activity.findViewById(R.id.report_issue_title);
+		if (t == null)
+			return false;
+		else {
+			s = t.getText().toString();
+			if (s.compareTo("") == 0)
+				return false;
+
+			i.setTitle(s);
 		}
 
-		return false;
+		t = (EditText) activity.findViewById(R.id.report_issue_description);
+		if (t == null)
+			return false;
+		else {
+			s = t.getText().toString();
+			if (s.compareTo("") != 0)
+				i.setDescription(s);
+		}
+
+		return true;
 	}
-}
+
+	private OnClickListener dispatchTakePictureIntent = new OnClickListener() {
+
+		@Override
+		public void onClick(View v) {
+			Intent takePictureIntent = new Intent(
+					MediaStore.ACTION_IMAGE_CAPTURE);
+			if (takePictureIntent.resolveActivity(activity.getPackageManager()) != null) {
+				startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+			}
+		}
+	};
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == REQUEST_IMAGE_CAPTURE
+				&& resultCode == Activity.RESULT_OK) {
+			Bitmap imageBitmap = (Bitmap) data.getExtras().get("data");
+			ImageView image = new ImageView(activity);
+			image.setImageBitmap(imageBitmap);
+			setMargins(image, 5, 0, 0, 5);
+
+			((LinearLayout) activity.findViewById(R.id.report_issue_photos))
+					.addView(image);
+		}
+	}
+
+	private void setMargins(ImageView image, int left, int top, int right,
+			int bottom) {
+		// TODO
+	}
+};
--- VineyardClient/src/com/formichelli/vineyard/VineyardMainActivity.java
+++ VineyardClient/src/com/formichelli/vineyard/VineyardMainActivity.java
@@ -13,6 +13,7 @@ public class VineyardMainActivity extends ActionBarActivity implements
 	private static final String TAG = "VineyardMainActivity";
 
 	Fragment mainFragment, issuesFragment, tasksFragment, settingsFragment;
+	public ReportIssueFragment reportIssueFragment;
 	Menu menu;
 
 	/**
@@ -113,15 +114,17 @@ public class VineyardMainActivity extends ActionBarActivity implements
 		return super.onCreateOptionsMenu(menu);
 	}
 
-//	@Override
-//	public boolean onOptionsItemSelected(MenuItem item) {
-//		// Handle action bar item clicks here. The action bar will
-//		// automatically handle clicks on the Home/Up button, so long
-//		// as you specify a parent activity in AndroidManifest.xml.
-//		 int id = item.getItemId();
-//		 if (id == R.id.action_settings) {
-//		 return true;
-//		 }
-//		return super.onOptionsItemSelected(item);
-//	}
+	// @Override
+	// public boolean onOptionsItemSelected(MenuItem item) {
+	// // Handle action bar item clicks here. The action bar will
+	// // automatically handle clicks on the Home/Up button, so long
+	// // as you specify a parent activity in AndroidManifest.xml.
+	// int id = item.getItemId();
+	// if (id == R.id.action_settings) {
+	// return true;
+	// }
+	// return super.onOptionsItemSelected(item);
+	// }
+
+	
 }
