f42a3b0a37907252058e949ad990cfe3f3b25c4b
Daniele Formichelli <daniele.formichelli@gmail.com>
2014-05-30 15:27:57 +0200

    Fixed all groups view when a group has no workers.

--- VineyardWebClient/js/group/all.js
+++ VineyardWebClient/js/group/all.js
@@ -1,16 +1,15 @@
 var insertGroup = function (group, $container) {
 	var padding = 10;
-	
+
 	var row = '<tr><td class="first-col" style="padding-left: ' + padding + 'px;">' +
-		'<a href="/group/' + group.id + '">' + group.name + '</a></td>' + 
-		'<td>' + group.description + '</td>' + 
-		'<td>' + group.workers.length + '</td></tr>';
+		'<a href="/group/' + group.id + '">' + group.name + '</a></td>' +
+		'<td>' + group.description + '</td><td>' + (group.workers != null ? group.workers.length : '0') + '</td></tr>';
 
     $container.append(row);
 };
 
 var loadList = function () {
-    
+
     var requestedUrl = vineyard.config.serverUrl + "group/" ;
     $.getJSON(requestedUrl, function (groups) {
         $.each(groups, function (index, group) {
